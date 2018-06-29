c95689afac4ea08576d9736ba84c73dcb7d6ea2b
Chen Bin <chen.6133@osu.edu>
2018-06-28 23:59:55 -0400

    change direct registration to chain registration

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -2,15 +2,17 @@
 // Modified by Bin Chen 6/28/2018 - Implemented registerNumButtons function
 
 function registerNumButtons(numberArea, view) {
-	var k = function () {
-		numberArea.append(this.value);
-		view.updateNumberView();
+	var appendNum = function () {
+		numberArea.append(this.value);	
+		view.updateNumberView();		
 	};	
+	
+	
 	
 	var elementList = document.getElementsByClassName("number");
 	
 	for (let i = 0; i < elementList.length; ++i)
-		elementList[i].onclick = k;
+		elementList[i].addEventListener("click", appendNum, false);
 }
 	
 function registerButtons(numberArea, resultArea, view) {
