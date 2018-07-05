b9899fc72025cb2c0f1a57019800198c39bc4be8
Houyi Fan <fan.685@osu.edu>
2018-07-05 01:13:29 -0400

    updated history

--- javascripts/model/history.js
+++ javascripts/model/history.js
@@ -2,6 +2,30 @@
 function History(){
     this.entries = [];
 }
+
+History.prototype.setEntry = function (expression, result) {
+	var entry = {
+    	exp: "",
+    	res: 0
+    };
+	entry.exp = expression;
+	entry.res = result;
+	return entry;
+}
+
+History.prototype.add = function (entry) {
+	this.entries.push(entry);	
+}
+
+History.prototype.html = function () {
+
+	var html = "";
+	for (var i = 0; i < this.entries.length; ++i) {
+		console.log(this.entries[i].exp + ", " + this.entries[i].res);
+ 	     html += "<div class='item' value=" + i + "><span id=" + i + ">" + this.entries[i].exp + " = " + this.entries[i].res + "</span></div>";
+ 	     html += "</br>";
+    }
+}
 /**
  * Clears the history
  *
